d6aec2a069a0666fa0f7f50b76f913fa1d7729c5
lkarrie <lkarrie@163.com>
2023-11-23 22:49:05 +0800

    add search

--- api/image.go
+++ api/image.go
@@ -2,19 +2,42 @@ package api
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/docker/docker/api/types"
 	"github.com/gin-gonic/gin"
 )
 
+type listImageRequest struct {
+	Key string `form:"key"`
+}
 func (server *Server) listImage(ctx *gin.Context) {
 
+	var req listImageRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	
 	images, err := server.docker.listImages(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, images)
+	if (req.Key == "" ) {
+		ctx.JSON(http.StatusOK, images)
+	} else {
+		var searchRes = []types.ImageSummary{}
+		for _, image := range images {
+			for _, tag := range image.RepoTags {
+				if (strings.Contains(tag,req.Key)){
+					searchRes = append(searchRes, image)
+				}
+			}
+		}
+		ctx.JSON(http.StatusOK, searchRes)
+	}
 }
 
 type pullImageRequest struct {
